f2a865a7272e6451a9297a5b1815346f87429034
shubham Kumar Nimje <shubbham13@gmail.com>
2024-01-23 19:27:25 +0530

    DAO added

--- SchoolManagement/src/main/java/com/schoolManagement/pack/Model/Student.java
+++ SchoolManagement/src/main/java/com/schoolManagement/pack/Model/Student.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
--- SchoolManagement/src/main/java/com/schoolManagement/pack/Model/Teacher.java
+++ SchoolManagement/src/main/java/com/schoolManagement/pack/Model/Teacher.java
@@ -12,7 +12,7 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Teacher {
+public class  Teacher{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
