8893f9ed5a7a9b2739465b8a5fefa73732dd4148
shubham Kumar Nimje <shubbham13@gmail.com>
2024-02-06 19:58:58 +0530

    Quiz and Question service added to admin and user counter parts.And server changed to Spring Cloud Eureka and Apis interacting through OpenFeign.

--- SchoolManagement/src/main/java/com/schoolManagement/pack/SchoolManagementApplication.java
+++ SchoolManagement/src/main/java/com/schoolManagement/pack/SchoolManagementApplication.java
@@ -2,10 +2,12 @@ package com.schoolManagement.pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SchoolManagementApplication {
 
 	public static void main(String[] args) {
